a2cbc584944c91426f927eb9fe8960f941d21009
chikiBamboni <alexander.palatkin@gmail.com>
2021-04-29 19:32:26 +0300

    lesson 07HW

--- src/main/java/lesson07/Cat.java
+++ src/main/java/lesson07/Cat.java
@@ -5,6 +5,15 @@ import java.util.Random;
 public class Cat {
 
     Random random = new Random();
+
+    private int max_satiety = 20;
+
+    public int getMax_satiety() {
+        return max_satiety;
+    }
+
+    private int satiety;
+
     private final String name;
 
     public Cat(String name) {
@@ -12,6 +21,29 @@ public class Cat {
     }
 
     public void eat(Plate plate) {
-        plate.decreaseFood(random.nextInt(4) + 3);
+
+        if (satiety < max_satiety) {
+            int count = 0;
+            int randomNumb = random.nextInt(4) + 8;
+            satiety += randomNumb;
+            if (satiety > max_satiety){
+                count = satiety - max_satiety;
+                satiety -= count;
+                plate.decreaseFood(randomNumb - count);
+                System.out.println(name + " сыт");
+            } else {
+                plate.decreaseFood(randomNumb);
+            }
+
+        }
+
+    }
+
+    public int getSatiety() {
+        return satiety;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/main/java/lesson07/Test.java
+++ src/main/java/lesson07/Test.java
@@ -1,19 +1,64 @@
 package lesson07;
 
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) throws InterruptedException {
-        Cat cat = new Cat("Martin");
-        Plate plate = new Plate(100);
+
+        Scanner scanner = new Scanner(System.in);
+
+        Cat[] cats = new Cat[5];
+        cats[0] = new Cat("Sheldon");
+        cats[1] = new Cat("Asty");
+        cats[2] = new Cat("Martin");
+        cats[3] = new Cat("Gin");
+        cats[4] = new Cat("Jamal");
+        Plate plate = new Plate(50);
 
         plate.printInfo();
 
-        while (plate.getFoodCount() > 0) {
-            cat.eat(plate);
-            plate.printInfo();
-            Thread.sleep(3000);
+        boolean feeding = true;
 
-            //метод котики голодают
+        while (feeding) {
+            for (Cat cat : cats) {
+                if (plate.getFoodCount() == 0) {
+                    System.out.println("Введите количество еды, чтобы пополнить тарелку или введите \"0\", если не хотите");
+                    if (scanner.hasNextInt()) {
+                        int addFood = scanner.nextInt();
+                        if (addFood == 0) {
+                            System.out.println("Здоровее будут, до свидания!");
+                            feeding = false;
+                            break;
+                        } else if (addFood > 0) {
+                            plate.setFoodCount(addFood);
+                        } else {
+                            System.out.println("Мне нечего вам сказать, до свидания.");
+                            feeding = false;
+                            break;
+                        }
+                    } else {
+                        System.out.println("Мне нечего вам сказать, до свидания.");
+                        feeding = false;
+                        break;
+                    }
+                } else if (cat.getSatiety() < cat.getMax_satiety()) {
+                    cat.eat(plate);
+                    plate.printInfo();
+                    Thread.sleep(500);
+                } else {
+                    feeding = false;
+                }
+            }
+            if (cats[cats.length - 1].getSatiety() == cats[cats.length - 1].getMax_satiety()) {
+                System.out.println("Все котики сыты");
+                break;
+            }
+        }
+
+        for (Cat cat : cats) {
+            System.out.println("У кота \"" + cat.getName() + "\" сытость заполнена на: " +
+                    cat.getSatiety() + " из " + cat.getMax_satiety());
         }
 
     }
